dcc4807dfdd11560f2ded4caa664d32bd911a1c8
Lpiat <lucienpiat33@gmail.com>
2024-05-12 18:31:13 +0200

    Add basic functions

--- exo_1.py
+++ exo_1.py
@@ -4,7 +4,44 @@ Lucien PIAT M1 Bioinfo 15/05/2024
 Exercice 1 - Des arbres, des billes et des collisions
 """
 
-def creer_boite(longueur, hauteur, orientation, billes, SO, NE): 
+def inserer_ab(A, pere, fils, etiquette=None, type_fils=None):
+    if fils is None:
+        return
+    if not pere is None:
+        A[type_fils][pere] = fils
+        A['pere'][fils] = pere
+        A['fils_gauche'][fils] = None
+        A['fils_droit'][fils] = None
+        A['etiquettes'][fils] = etiquette
+
+
+def creer_arbre_vide():
+    return {
+    'racine' : None,
+    'etiquettes' : {},
+    'fils_droit' : {},
+    'fils_gauche' : {},
+    'pere' : {}
+    }
+
+def creer_arbre(racine=None, etiquette=None):
+    res = creer_arbre_vide()
+    inserer_racine(res, racine, etiquette)
+    return res
+
+def inserer_racine(A, racine, etiquette=None):
+    A['racine'] = racine
+    inserer_ab(A, None, racine, etiquette, type_fils=None)
+
+
+def inserer_fils_gauche(A, pere, fils, etiquette=None):
+    inserer_ab(A, pere, fils, etiquette=etiquette, type_fils='fils_gauche')
+
+def inserer_fils_droit(A, pere, fils, etiquette=None):
+    inserer_ab(A, pere, fils, etiquette=etiquette, type_fils='fils_droit')
+
+
+def nouvelle_etiquette(longueur, hauteur, orientation, SO, NE, billes=[]): 
     return {
         "longueur":longueur, 
         "hauteur":hauteur, 
@@ -14,14 +51,8 @@ def creer_boite(longueur, hauteur, orientation, billes, SO, NE):
         "NE" : NE
         }
 
-longueur = 3
-hauteur = 3
-orientation = "verticale"
-billes = []
-SO = (1,0)
-NE = (2,3)
+def construire_arbre_collision(H,L,nb_etape):
+    etiquette_racine = nouvelle_etiquette(L, H, "verticale", (0,0), (L,H))
+    A = creer_arbre(etiquette = etiquette_racine)
 
-
-
-
-print(creer_boite(longueur, hauteur, orientation, billes, SO, NE))
+construire_arbre_collision(6,6,1)
